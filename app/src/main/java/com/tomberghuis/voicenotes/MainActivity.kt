b15567b41645e9ca3db0a9bf256c81ff0feb3cad
Tom Berghuis <thomas.berghuis@gmail.com>
2018-09-28 19:39:32 +1000

    .

--- app/src/main/java/com/tomberghuis/voicenotes/MainActivity.kt
+++ app/src/main/java/com/tomberghuis/voicenotes/MainActivity.kt
@@ -8,6 +8,9 @@ import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 
+
+// this comment is a test
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
